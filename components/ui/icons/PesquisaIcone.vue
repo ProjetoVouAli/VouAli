b68d90d5cad609d12d0d296fccbcbeeaf67a7f89
Victor Stark <luan.victtor@hotmail.com>
2025-06-20 01:11:03 -0300

    feat: ajustar propriedades do ícone de pesquisa para tamanhos e espessuras de linha mais finas

--- components/ui/icons/PesquisaIcone.vue
+++ components/ui/icons/PesquisaIcone.vue
@@ -11,6 +11,7 @@
         stroke-linecap="round"
         stroke-linejoin="round"
         xmlns="http://www.w3.org/2000/svg"
+        style="vertical-align: middle; line-height: 1;"
         :class="className"
     >
         <circle cx="11" cy="11" r="8" />
@@ -22,11 +23,11 @@
 defineProps({
     size: {
         type: [Number, String],
-        default: 24
+        default: 16  // Reduzido de 24 para 16
     },
     strokeWidth: {
         type: [Number, String],
-        default: 2
+        default: 1.5  // Reduzido de 2 para 1.5 para linhas mais finas
     },
     className: {
         type: String,
